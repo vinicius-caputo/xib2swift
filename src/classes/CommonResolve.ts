a0a97872a2c8a715d74ad8775790b774113d05cb
vinicius-caputo <viniciuccastrov@gmail.com>
2023-03-31 11:12:36 -0300

    [improvement] - fix in Resolve.color

--- src/classes/CommonResolve.ts
+++ src/classes/CommonResolve.ts
@@ -5,19 +5,19 @@ export class Resolve {
 
     public static Color(node: XibNode): string {
         let declaration: string = '';
-        if (node.attrs.customColorSpace == 'genericGamma22GrayColorSpace') {
+        if (node.attrs.customColorSpace == 'genericGamma22GrayColorSpace' || node.attrs.colorSpace == 'genericGamma22GrayColorSpace') {
             declaration = `UIColor(cgColor: CGColor(genericGrayGamma2_2Gray: ${node.attrs.white}, alpha: ${node.attrs.alpha}))`;
         }
-        else if (node.attrs.customColorSpace == 'sRGB') {
+        else if (node.attrs.customColorSpace == 'sRGB' || node.attrs.colorSpace == 'sRGB') {
             declaration = `UIColor(cgColor: CGColor(srgbRed: ${node.attrs.red}, green: ${node.attrs.green}, blue: ${node.attrs.blue}, alpha: ${node.attrs.alpha}))`
         }
-        else if (node.attrs.customColorSpace == 'displayP3') {
+        else if (node.attrs.customColorSpace == 'displayP3' || node.attrs.colorSpace == 'displayP3') {
             declaration = `UIColor(displayP3Red: ${node.attrs.red}, green: ${node.attrs.green}, blue: ${node.attrs.blue}, alpha: ${node.attrs.alpha})`
         }
-        else if (node.attrs.customColorSpace == 'calibratedWhite') {
+        else if (node.attrs.customColorSpace == 'calibratedWhite' || node.attrs.colorSpace == 'calibratedWhite') {
             declaration = `UIColor(white: ${node.attrs.white}, alpha: ${node.attrs.alpha})`
         }
-        else if (node.attrs.customColorSpace == 'calibratedRGB') {
+        else if (node.attrs.customColorSpace == 'calibratedRGB' || node.attrs.colorSpace == 'calibratedRGB') {
             declaration = `UIColor(red: ${node.attrs.red}, green: ${node.attrs.green}, blue: ${node.attrs.blue}, alpha: ${node.attrs.alpha})`
         }
         else if (node.attrs.systemColor != undefined)  {
